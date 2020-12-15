008f1c9224ac2b24293d5fc6b5aad19ceac8f516
horjurares <horjurares@gmail.com>
2020-12-15 11:35:49 +0200

    Enet ma distruge

--- client.py
+++ client.py
@@ -1,7 +1,8 @@
 import enet
 
-host = enet.Host(None, 1, 0, 0, 0)
-peer = host.connect(enet.Address(b"localhost", 54301), 1)
+host = enet.Host(enet.Address("188.27.45.124", 54301), 1, 0, 0, 0)
+peer = host.connect(enet.Address("82.76.59.55", 54301), 1)
+
 while 1:
     event = host.service(1000)
     if event.type == enet.EVENT_TYPE_CONNECT:
--- host.py
+++ host.py
@@ -1,7 +1,6 @@
 import enet
 
-host = enet.Host(enet.Address(b"localhost", 54301), 10, 0, 0, 0)
-print(enet.Address(b"localhost", 54301))
+host = enet.Host(enet.Address("188.27.45.124", 54301), 10, 0, 0, 0)
 
 while 1:
     # Wait 1 second for an event
